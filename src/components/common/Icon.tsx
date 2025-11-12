a827eca3ae7daedfdac47bee6e5ddb0305cbab16
fnzl54 <rnjscksdud1@naver.com>
2025-11-12 14:43:36 +0900

    feat(#9): 문제 모아보기 페이지 퍼블리싱

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -90,35 +90,13 @@ const Icon = ({ name, size = 28, className = "", onClick }: IconProps) => {
       </svg>
     ),
     calendar: (
-      <svg
+      <img
+        src="/icon/calender.svg"
+        alt="book"
         width={size}
         height={size}
-        viewBox="0 0 28 28"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect
-          x="3.5"
-          y="6.5"
-          width="21"
-          height="18"
-          rx="1.5"
-          stroke="currentColor"
-          strokeWidth="2"
-        />
-        <path
-          d="M3.5 10.5H24.5"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-        />
-        <path
-          d="M8 3.5V8.5M20 3.5V8.5"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-        />
-      </svg>
+        className={className}
+      />
     ),
     /* [제거됨]
     check: (
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,11 @@
 // Layout Components
-export { Header, Footer } from './layout';
+export { Header, Footer } from "./layout";
 
 // Common Components
-export { Modal, QuizResultModal, Input, Card, Icon, Button } from './common';
-export { default as LoginModal } from './common/LoginModal';
-export { default as QuizCreateModal } from './common/QuizCreateModal';
-export { default as ProgressBar } from './common/ProgressBar';
+export { Modal, QuizResultModal, Input, Card, Icon, Button } from "./common";
+export { default as LoginModal } from "./common/LoginModal";
+export { default as QuizCreateModal } from "./common/QuizCreateModal";
+export { default as ProgressBar } from "./common/ProgressBar";
+export { default as DateCard } from "./common/DateCard";
+export { default as QuizCard } from "./common/QuizCard";
+export { default as UnauthorizedPage } from "./common/UnauthorizedPage";
