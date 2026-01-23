e771f43f4fa568197b3cd6c62d906eff0841ef92
fnzl54 <rnjscksdud1@naver.com>
2026-01-23 10:14:04 +0900

    refactor(#61): 문제 모아보기 페이지 카드 크기 수정

--- src/components/common/DateCard.tsx
+++ src/components/common/DateCard.tsx
@@ -9,13 +9,11 @@ const DateCard = ({ date, onClick }: DateCardProps) => {
   return (
     <button
       onClick={onClick}
-      className="bg-white border border-gray-300 hover:border-primary px-l py-10 rounded-[12px] w-full transition-colors"
+      className="bg-white border border-[#ededed] hover:border-primary px-[26px] py-10 rounded-[12px] w-full transition-colors shadow-[4px_4px_12px_0px_rgba(0,0,0,0.04)] max-md:px-4 max-md:py-8"
     >
-      <div className="flex flex-col gap-3 items-center">
-        <div className="flex gap-1 items-center">
-          <Icon name="calendar" size={28} className="text-error" />
-          <span className="text-body1-medium text-gray-900">{date}</span>
-        </div>
+      <div className="flex gap-1 items-center justify-center">
+        <Icon name="calendar" size={28} className="text-error max-md:!w-6 max-md:!h-6" />
+        <span className="text-body1-medium text-gray-900 max-md:!text-[16px] max-md:!font-medium max-md:!leading-[1.4]">{date}</span>
       </div>
     </button>
   );
