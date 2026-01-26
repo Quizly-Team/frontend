d8f25533b1fed4e4c138693a984a53112858a63c
fnzl54 <rnjscksdud1@naver.com>
2026-01-26 15:35:27 +0900

    feat(#64): 문제 풀이 중 이탈 방지 모달 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@
 export { Header, Footer } from "./layout";
 
 // Common Components
-export { Modal, QuizResultModal, Input, Card, Icon, Button } from "./common";
+export { Modal, QuizResultModal, QuizExitConfirmModal, Input, Card, Icon, Button } from "./common";
 export { default as QuizCreateModal } from "./common/QuizCreateModal";
 export { default as ProgressBar } from "./common/ProgressBar";
 export { default as DateCard } from "./common/DateCard";
